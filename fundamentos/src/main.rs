e7cd13cee6d4284c58b355e2edd9edf5e26ae5e2
LeonardoBaquero <42071926+LeonardoBaquero@users.noreply.github.com>
2025-12-07 21:08:29 -0500

    Primer commit proyecto Rust

---
+++ fundamentos/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    //let edad = 30; 
+
+   let mensaje = "Hola a todos";
+
+    //let valor = 20500; 
+
+    //let valor2 = 19.88; 
+
+    //let actividad = true;  
+
+    println!("{}", mensaje);
+     
+}
