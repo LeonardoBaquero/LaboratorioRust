c5085f7243e4185ef5b46b0c9b33f5743e94b9b9
LeonardoBaquero <42071926+LeonardoBaquero@users.noreply.github.com>
2026-02-01 21:13:04 -0500

    video 9 Rust

--- hola_rust/src/main.rs
+++ hola_rust/src/main.rs
@@ -2,13 +2,33 @@
 
 fn main() {
     
-    let x: i32 = 10;
+    /* let x: i32 = 10;
     println!("{}",x);
     let x: String = x.to_string();
-    println!("{}",x);
- 
-}
+    println!("{}",x); */ 
 
-// 27 de enero por la noche 
+    // Vector con datos dinámicos 
+
+   /*let mut numeros=Vec::with_capacity(3); 
+
+   numeros.push(15);
+   numeros.push(23); 
+   numeros.push(4);
+   numeros.push(4);
+   numeros.push(48);
+
+   println!("{:?}", numeros); */ 
+
+   
+    let edad = -5; 
+
+    if edad < 0{
+        println!("La edad no puede ser negativa: {}", edad);
+        
+    }
+}
+ 
+
+
 
   
